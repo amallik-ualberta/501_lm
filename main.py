c398f7de5855be1e124aa50767d16f9e11cba659
amallik-ualberta <amallik@ualberta.ca>
2019-10-06 19:20:17 -0600

    second commit

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
-import csv
+
 import glob
 import os
-import re
+
 import sys
 
 if len(sys.argv) == 1:
-    folder_path = input("Enter Folder Path of Wiki files: ")
+    folder_path = input("Enter Folder Path of Training files: ")
 else:
     folder_path = sys.argv[1]
 
-for filename in glob.glob(os.path.join(folder_path, "*.wiki")):
-    with open(filename, "r", encoding="utf8") as f:
-        contents = f.read()
-        fact_store = []
-        name = get_facts_from_infobox_film_content()
-        if name is None:
-            name = get_facts_from_text_contents()
-        else:
-            get_facts_from_text_contents()
-        write_to_tsv(fact_store, folder_path, name)
+for filename in glob.glob(os.path.join(folder_path, "*.tra")):
+    f = open (filename, "r")
+    contents = f.read()
+    print(contents)
